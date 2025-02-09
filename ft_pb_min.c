ea5a6acda12b944ac2ce5ac13ec0dc3e9b52e387
Mohamed Houssas <mhoussas@e3r9p3.1337.ma>
2025-02-09 15:49:42 +0100

    ok

--- ft_pb_min.c
+++ ft_pb_min.c
@@ -6,91 +6,13 @@
 /*   By: mhoussas <mhoussas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 13:01:51 by mhoussas          #+#    #+#             */
-/*   Updated: 2025/02/08 15:50:28 by mhoussas         ###   ########.fr       */
+/*   Updated: 2025/02/09 15:24:43 by mhoussas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-static int	ft_get(t_list *lst)
-{
-	static int	*lst_max;
-	t_list		*head;
-	int			aid;
-	int			i;
-
-	head = lst;
-	if (!lst_max)
-	{
-		lst_max = ft_malloc(sizeof(int)*3);
-		aid = *(lst->content);
-		lst = lst->next;
-		while (lst)
-		{
-			if (*(lst->content) > aid)
-				aid = *(lst->content);
-			lst = lst->next;
-		}
-		lst_max[0] = aid;
-
-		lst = head;
-		aid = *(lst->content);
-		lst = lst->next;
-		while (lst)
-		{
-			if (aid == lst_max[0] || (*(lst->content) != lst_max[0] && *(lst->content) > aid))
-				aid = *(lst->content);
-			lst = lst->next;
-		}
-		lst_max[1] = aid;
-
-		lst = head;
-		aid = *(lst->content);
-		lst = lst->next;
-		while (lst)
-		{
-			if (aid == lst_max[0] || aid == lst_max[1] || (*(lst->content) != lst_max[0] && *(lst->content) != lst_max[1] && *(lst->content) > aid))
-				aid = *(lst->content);
-			lst = lst->next;
-		}
-		lst_max[2] = aid;
-	}
-	lst = head;
-	i = 0;
-	while (lst)
-	{
-		if (*(lst->content) != lst_max[0] && *(lst->content) != lst_max[1] && *(lst->content) != lst_max[2])
-			break ;
-		i++;
-		lst = lst->next;
-	}
-	return (i);
-}
-
-int ft_get_min(t_list *lst)
-{
-	int	aid;
-	int	i;
-	int	j;
-
-	i = 0;
-	j = 0;
-	aid = *(lst->content);
-	while (lst)
-	{
-		if (aid > *(lst->content))
-		{
-			aid = *(lst->content);
-			j = i;
-		}
-		lst = lst->next;
-		i++;
-	}
-	return (j);
-}
-
 void	ft_pb_min(t_stackes **stackes)
 {
-	ft_pa_top(stackes, ft_get((*stackes)->stack_a));
-	ft_pb(*stackes);
+	pass;
 }
--- ft_sieve.c
+++ ft_sieve.c
@@ -6,83 +6,8 @@
 /*   By: mhoussas <mhoussas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 16:40:08 by mhoussas          #+#    #+#             */
-/*   Updated: 2025/02/08 18:14:45 by mhoussas         ###   ########.fr       */
+/*   Updated: 2025/02/09 15:23:38 by mhoussas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
-
-static int	ft_min(int n, int n2)
-{
-	if (n > n2)
-		return (n2);
-	return (n);
-}
-
-static t_list	*ft_aid(t_list *lst)
-{
-	t_list	*res;
-	int		len_a, len_b;
-	char	aid2;
-	int		aid;
-
-	res = NULL;
-	len_a = 0;
-	len_b = 0;
-	while (lst)
-	{
-		if (!ft_strncmp((lst->content), "ra", 2))
-			len_a++;
-		else
-			len_b++;
-		lst = lst->next;
-	}
-	aid = ft_min(len_a, len_b);
-	while (aid--)
-		ft_lstadd_back(&res, ft_lstnew("rr"));
-	if (len_a > len_b)
-		aid2 = "ra";
-	else
-		aid2 = "rb";
-	aid = len_a - aid + len_b - aid;
-	while (aid--)
-		ft_lstadd_back(&res, ft_lstnew(aid2));
-	return (res);
-}
-
-t_list	*ft_sieve(t_list *instructions)
-{
-	t_list	*res;
-	t_list	*aid;
-	t_list	*lst;
-	int		flag;
-
-	flag = 0;
-	aid = NULL;
-	res = NULL;
-	lst = instructions;
-	while (lst)
-	{
-		if (!flag && (!ft_strncmp((lst->content), "ra", 2) || !ft_strncmp((lst->content), "ra", 2)))
-		{
-			flag = 1;
-			ft_lstadd_back(&aid, ft_lstnew(lst->content));
-		}
-		else if (flag)
-		{
-			if (!ft_strncmp((lst->content), "ra", 2) || !ft_strncmp((lst->content), "ra", 2))
-				ft_lstadd_back(&aid, ft_lstnew(lst->content));
-			else
-			{
-				flag = 0;
-				ft_lstadd_back(&res, ft_aid(aid));
-				ft_lstadd_back(&res, ft_lstnew(lst->content));
-				aid = NULL;
-			}
-		}
-		else
-			ft_lstadd_back(&res, ft_lstnew(lst->content));
-		lst = lst->next;
-	}
-	return (res);
-}
